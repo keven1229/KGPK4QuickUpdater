9c4609e36a4305f253e212d40934fe8251ef5067
keven <keven@kevenidc.com>
2022-03-31 21:09:47 +0800

    Init

---
+++ KGPK4QuickUpdater/frmMain.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace KevenIDC.JX3.KGPK4QuickUpdater
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblProgramVersion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblProgramVersion
+            // 
+            this.lblProgramVersion.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblProgramVersion.AutoSize = true;
+            this.lblProgramVersion.Location = new System.Drawing.Point(741, 429);
+            this.lblProgramVersion.Name = "lblProgramVersion";
+            this.lblProgramVersion.Size = new System.Drawing.Size(47, 12);
+            this.lblProgramVersion.TabIndex = 0;
+            this.lblProgramVersion.Text = "Version";
+            this.lblProgramVersion.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblProgramVersion);
+            this.Name = "frmMain";
+            this.Text = "KGPK4更新包快速下载器";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblProgramVersion;
+    }
+}
+
---
+++ KGPK4QuickUpdater/frmMain.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KevenIDC.JX3.KGPK4QuickUpdater
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            this.lblProgramVersion.Text = Application.ProductVersion;
+        }
+    }
+}
